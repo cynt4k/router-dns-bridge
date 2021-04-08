be4b630aec56db4c9ee21275dec4abfa4e7ed6fe
cynt4k <me@cynt4k.de>
2021-04-08 15:42:37 +0200

    add configs
    Signed-off-by: cynt4k <me@cynt4k.de>

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -4,8 +4,31 @@ var (
 	config = &Config{}
 )
 
+type RouterOptions struct {
+	AllowedIPs []string `yaml:"allowedIps"`
+}
+type Router struct {
+	APIKey   string        `yaml:"apiKey"`
+	Domain   string        `yaml:"domain"`
+	Provider string        `yaml:"provider"`
+	Options  RouterOptions `yaml:"routerOptions"`
+}
+type ProviderPowerdns struct {
+	URL    string `yaml:"url"`
+	APIKey string `yaml:"apiKey"`
+}
+type Providers struct {
+	Powerdns ProviderPowerdns `yaml:"powerdns"`
+}
+type API struct {
+	Host string `yaml:"host"`
+	Port int    `yaml:"port"`
+}
 type Config struct {
-	DevMode bool `yaml:"devMode"`
+	DevMode   bool              `yaml:"devMode"`
+	API       API               `yaml:"api"`
+	Providers Providers         `yaml:"providers"`
+	Routers   map[string]Router `yaml:"routers"`
 }
 
 func GetConfig() *Config {
